05244e35dc8f5e826ac6944b996ce7e50b4affa5
Lisa <user@MacBook-Pro--Lisa.local>
2025-08-08 23:58:24 +0300

    create base append elements dl list

--- list/list.cpp
+++ list/list.cpp
@@ -1,46 +1,96 @@
 #include "list.hpp"
 
 template <typename T>
-typename List<T>::Node& List<T>::Node::operator=(const Node& right)
+List<T>::Node::Node(T val): next{nullptr}, prev{nullptr}, value{val}
+    {   }
+
+template <typename T>
+List<T>::List(): head{nullptr}, tail{nullptr}, size{0}
+    {   }
+
+template <typename T>
+List<T>::List(const List& right)
+{
+    if(this == &right)
+        return;
+
+    head.reset(right.head.get());
+    tail.reset(right.tail.get());
+    size = right.size;
+}
+
+template <typename T>
+List<T>::List(List&& right) noexcept
 {
     if(&right == this)
-        return *this;
+        return;
 
-    Node copy{right};
+    head = nullptr;
+    tail = nullptr;
+    head = right.head;
+    tail = right.tail;
+    size = right.size;
+    right.head = nullptr;
+    right.tail = nullptr;
+}
+
+template <typename T>
+void List<T>::swap(List& obj) noexcept
+{
+    std::swap(head, obj.head);
+    std::swap(tail, obj.tail);
+    std::swap(size, obj.size);
+}
+
+template <typename T>
+List<T>& List<T>::operator=(const List& right)
+{
+    List<T> copy{right};
     swap(copy);
     return *this;
 }
 
 template <typename T>
-void List<T>::Node::swap(Node& obj) noexcept
+List<T>& List<T>::operator=(List&& right) noexcept
 {
-    std::swap(obj.next, next);
-    std::swap(obj.prev, prev);
-    std::swap(obj.value, value);
+    List<T> copy{right};
+    swap(copy);
+    return *this;
 }
 
 template <typename T>
-List<T>::List()
+void List<T>::push_back(T val)
 {
-    // Node nd1{10};
-    // Node nd2{};
-    // nd1 = nd1;
+    auto new_node = make_shared<Node>(val);
+    if(!head)
+    {
+        head = new_node;
+        tail = new_node;
+        new_node->prev = head;
+    }
+    else 
+    {
+        new_node->prev = tail;
+        head->next = new_node;
+        tail = new_node;
+    }
+    size++;
 }
 
 template <typename T>
-List<T>::Node::Node(): value{T{}}, next{nullptr}, prev{nullptr}
-    {   }
-
-template <typename T>
-List<T>::Node::Node(T val): value{val}, next{nullptr}, prev{nullptr}
-    {   }
-
-template <typename T>
-List<T>::Node::Node(const Node& right): next{right.next}, prev{right.prev}, value{right.value}
-    {   }
+List<T>::List(std::initializer_list<T> args)
+{
+    for(auto i = args.begin(); i != args.end(); i++)
+    {
+        push_back(*i);
+    }
+}
 
 int main(void)
 {
-    List<int> ls{};
+    List<int> lst{};
+    List<int> lst1{1, 2, 3};
+    lst.push_back(10);
+    lst.push_back(20);
     return 0;
 }
--- list/list.hpp
+++ list/list.hpp
@@ -2,27 +2,17 @@
 
 #include <iostream>
 #include <memory>
+#include <initializer_list>
 
 using namespace std;
 
 template <typename T>
 class List {
-    class Node {
+    struct Node {
         shared_ptr<Node> next{nullptr};
         shared_ptr<Node> prev{nullptr};
         T value;
-
-    public:
-        Node();
         Node(T val);
-        Node(const Node& right);
-        Node(Node&& right) noexcept;
-        Node& operator=(const Node& right);
-        Node& operator=(Node&& right) noexcept;
-        T& get_value() const;
-        void linked_node(T val);
-        void swap(Node& obj) noexcept;
-        ~Node() = default;
     };
 
     shared_ptr<Node> head{nullptr};
@@ -30,5 +20,14 @@ class List {
     int size = 0;
 public:
     List();
+    List(std::initializer_list<T> args);
+    List(const List<T>& right);
+    List(List<T>&& right) noexcept;
+    List& operator=(const List<T>& right);
+    List& operator=(List<T>&& right) noexcept;
+    void swap(List& obj) noexcept;
+    int get_size() const;
+    void push_back(T val);
+    ~List() = default;
 };  
 
