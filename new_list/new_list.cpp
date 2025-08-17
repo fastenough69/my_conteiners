2b6375edb8516db07d6c2b5949ae915def2968ef
fastenough69 <van.lisenkov@yandex.ru>
2025-08-17 14:24:56 +0300

    new list new options (index, pop front/back, constructor by init_list)

--- new_list/new_list.cpp
+++ new_list/new_list.cpp
@@ -31,6 +31,7 @@ void List<T>::push_back(T val)
         last->next = new_node;
         tail = new_node;
     }
+    size++;
 }
 
 template <typename T>
@@ -42,6 +43,7 @@ void List<T>::print_list() const
         std::cout << curr->value << ' ';
         curr = curr->next;
     }
+    std::cout << std::endl;
 }
 
 template <typename T>
@@ -64,6 +66,24 @@ List<T>::List(List&& right) noexcept
     head = right.head;
     tail = right.tail;
     size = right.size;
+}
+
+template <typename T>
+List<T>::List(std::initializer_list<T> args)
+{
+    for(auto i = args.begin(); i != args.end(); i++)
+    {
+        push_back(*i);
+    }
+}
+
+template <typename T>
+List<T>::List(size_t quainity, T val)
+{
+    for(int i{}; i < quainity; i++)
+    {
+        push_back(val);
+    }
 }
 
 template <typename T>
@@ -105,15 +125,111 @@ void List<T>::push_front(T val)
         head->prev = new_node;
         head = new_node;
     }
+    size++;
+}
+
+template <typename T>
+typename List<T>::Prox List<T>::operator[](int index)
+{
+    return Prox(this, index);
+}
+
+template <typename T>
+List<T>::Prox::Prox(List* arg, size_t idx)
+{
+    if(arg == nullptr)
+        return;
+
+    obj = arg;
+    index = idx;
+}
+
+template <typename T>
+std::shared_ptr<typename List<T>::Node> List<T>::Prox::search_node() const
+{
+    auto curr = obj->head;
+    int i{};
+    if(index > obj->size / 2)
+    {
+        curr = obj->tail.lock();
+        i = obj->size - 1;
+        for( ; i != index; i++)
+        {
+            curr = curr->prev.lock();
+        }
+    }
+    else
+    {
+        for( ; i < index; i++)
+        {
+            curr = curr->next;
+        }
+    }
+    return curr;
+}
+
+template <typename T>
+List<T>::Prox::operator T() const
+{
+    if(index >= obj->size)
+        throw "Index out of range\n";
+
+    auto curr = search_node();
+    return curr->value;
+}
+
+template <typename T>
+T List<T>::Prox::operator=(T right)
+{
+    if(index >= obj->size)
+        throw "Index out of range\n";
+
+    auto curr = search_node();
+    curr->value = right;
+    return curr->value;
+}
+
+template <typename T>
+void List<T>::pop_back()
+{
+    auto p_last = tail.lock();
+    tail = p_last->prev.lock();
+    auto last = tail.lock();
+    last->next = nullptr;
+    tail = last;
+    size--;
+}
+
+template <typename T>
+void List<T>::pop_front()
+{
+    head = head->next;
+    auto p_head = head->prev.lock();
+    p_head = nullptr;
+    head->prev = p_head;
+    size--;
 }
 
 int main(void)
 {
-    List<int> lst;
+    List<int> lst, lst2 = {1, 2, 3, 4}, lst3(5, 2);
     lst.push_back(10);
     lst.push_back(20);
     lst.push_back(30);
     lst.push_front(7);
+    lst.pop_back();
+    lst3.print_list();
+    lst2.print_list();
     lst.print_list();
+    lst.pop_front();
+    try
+    {
+        lst[1] = 35;
+        lst.print_list();
+    }
+    catch(const char* mes)
+    {
+        std::cerr << mes;
+    }
     return 0;
 }
--- new_list/new_list.hpp
+++ new_list/new_list.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <memory>
+#include <initializer_list>
 
 template <typename T>
 class List {
@@ -18,14 +19,30 @@ class List {
 
     void swap(List obj) noexcept;
 
+    class Prox {
+        List* obj;
+        size_t index;
+
+        std::shared_ptr<Node> search_node() const;
+    public:
+        Prox(List* arg, size_t idx);
+        operator T() const;
+        T operator=(T right);
+    };
+
 public:
     List();
     List(const List& right);
     List(List&& right) noexcept;
+    List(std::initializer_list<T> args);
+    List(size_t quainity, T val);
     List& operator=(const List& right);
     List& operator=(List&& right) noexcept;
+    Prox operator[](int index);
     ~List() = default;
     void push_back(T val);
     void push_front(T val);
+    void pop_back();
+    void pop_front();
     void print_list() const;
 };
